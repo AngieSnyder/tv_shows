a159b9246cfdba57d5bcbf842d0998bd27092ff0
AngieSnyder <Angie.Snyder@protonmail.com>
2018-09-21 11:46:02 -0400

    updated project

--- TvShow/src/tv/business/ShowDB.java
+++ TvShow/src/tv/business/ShowDB.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import tv.util.DBUtil;
 
-
 public class ShowDB implements ShowDAO {
 
 	private Connection getConnection() throws SQLException {
@@ -16,25 +15,62 @@ public class ShowDB implements ShowDAO {
 		Connection c = DriverManager.getConnection(dbURL, username, password);
 		return c;
 	}
+
 	@Override
-	public Show get(String genre) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Show> get(String genre) {
+		String sql = "select * from tvshow where genre = ?";
+		List<Show> shows = new ArrayList<>();
+		try (Connection connection = DBUtil.getConnection(); PreparedStatement ps = connection.prepareStatement(sql);) {
+			ps.setString(1, genre);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				String rating = rs.getString(3);
+				int length = rs.getInt(4);
+				String g = rs.getString(5);
+				String network = rs.getString(6);
+				Show s = new Show(id, name, rating, length, g, network);
+				shows.add(s);
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return shows;
 	}
 
 	@Override
-	public Show get(int length) {
-		
-		return null;
+	public List<Show> get(int length) {
+		String sql = "select * from tvshow where length = ?";
+		List<Show> shows = new ArrayList<>();
+		try (Connection connection = DBUtil.getConnection(); PreparedStatement ps = connection.prepareStatement(sql);) {
+			ps.setInt(1, length);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				int id = rs.getInt(1);
+				String name = rs.getString(2);
+				String rating = rs.getString(3);
+				int l = rs.getInt(4);
+				String genre = rs.getString(5);
+				String network = rs.getString(6);
+				Show s = new Show(id, name, rating, l, genre, network);
+				shows.add(s);
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return shows;
 	}
 
 	@Override
 	public List<Show> getAll() {
 		String sql = "SELECT * FROM tvshow";
 		List<Show> shows = new ArrayList<>();
-		try(Connection connection = DBUtil.getConnection();
-			PreparedStatement ps = connection.prepareStatement(sql);
-			ResultSet rs = ps.executeQuery()) {
+		try (Connection connection = DBUtil.getConnection();
+				PreparedStatement ps = connection.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery()) {
 			while (rs.next()) {
 				int id = rs.getInt(1);
 				String name = rs.getString(2);
@@ -45,12 +81,13 @@ public class ShowDB implements ShowDAO {
 				Show s = new Show(id, name, rating, length, genre, network);
 				shows.add(s);
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		return shows;
 	}
+
 // the below methods are not going to be implemented at this time.
 	@Override
 	public boolean add(Show s) {
--- TvShow/src/tv/util/DBUtil.java
+++ TvShow/src/tv/util/DBUtil.java
@@ -7,37 +7,37 @@ import java.sql.SQLException;
 import com.mysql.jdbc.Driver;
 
 public class DBUtil {
-    
-    private static Connection connection;
-    
-    private DBUtil() {}
 
-    public static Connection getConnection() throws SQLException {
-        
-            // set the db url, username, and password
-            String url = "jdbc:mysql://localhost:3306/tv_shows?autoReconnect=true&useSSL=false";
-            String username = "tv_shows_user";
-            String password = "sesame";
+	private static Connection connection;
 
-            // get and return connection
-            connection = DriverManager.getConnection(
-                    url, username, password);
-            //return connection;
-        
-        return connection;
-        
-    }
-    
-    public static void closeConnection()  throws SQLException{
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-            	System.out.println("Error closing connection!");
-                throw e;
-            } finally {
-                connection = null;                
-            }
-        }
-    }
+	private DBUtil() {
+	}
+
+	public static Connection getConnection() throws SQLException {
+
+		// set the db url, username, and password
+		String url = "jdbc:mysql://localhost:3306/tv_shows?autoReconnect=true&useSSL=false";
+		String username = "tv_shows_user";
+		String password = "sesame";
+
+		// get and return connection
+		connection = DriverManager.getConnection(url, username, password);
+		// return connection;
+
+		return connection;
+
+	}
+
+	public static void closeConnection() throws SQLException {
+		if (connection != null) {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				System.out.println("Error closing connection!");
+				throw e;
+			} finally {
+				connection = null;
+			}
+		}
+	}
 }
